80f37496aa7728790c94c2ae2ada5a0118d5c1e3
Esteban Benitez <esteban.benitez@asu.edu>
2020-08-22 20:42:37 -0700

    updating web app

--- app/run.py
+++ app/run.py
@@ -8,8 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sqlalchemy import create_engine
 import joblib
+from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('data/DisasterResponse.db', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -40,7 +40,8 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
+    print(df)
+    genre_counts = df.nunique().count()
     genre_names = list(genre_counts.index)
     
     # create visuals
